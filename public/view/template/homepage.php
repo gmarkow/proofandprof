b6ab29f1afe043ff7d97513e8d1994fae34ce7c5
Greg <gmarkowski@whitecloudcigs.com>
2017-09-08 15:29:37 -0400

    login page begining

--- public/view/template/homepage.php
+++ public/view/template/homepage.php
@@ -6,16 +6,20 @@ require_once(VIEW_DIR . 'head_logged_out.php');
 
 
 <div class="col-lg-6">
-  <a class="login-link" href="?route=login">
-    <img src="assets/images/signup.png">
-    <p>Sign Up</p>
+  <a href="?route=login">
+    <div id="signin-link" class="login-link">
+    <!-- <img src="assets/images/signup.png"> -->
+    <p class="login-text">Sign Up</p>
+    </div>
   </a>
 </div>
 
 <div class="col-lg-6">
-  <a class="login-link" href="?route=signin">
-    <img src="assets/images/login.png">
-    <p>Log In</p>
+  <a href="?route=signin">
+    <div  id="login-link" class="login-link">
+    <!-- <img src="assets/images/login.png"> -->
+    <p class="login-text">Log In</p>
+    </div>
   </a>
 </div>
 <div class="clearfix"></div>
